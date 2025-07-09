a8787303a94759c0f081fc341112a3e932e8eda1
William550-debug <williamnjoroge2019@gmail.com>
2025-07-09 10:26:12 +0300

    just finished up one the first page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
   return (
     <div>
-      
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+
+        </Routes>
+
+
+      </div>
+
     </div>
   )
 }
