cfee0ec7b5367dcab0be50631dc65829a019add8
William550-debug <williamnjoroge2019@gmail.com>
2025-07-10 23:18:44 +0300

    Jus finished up on the footer and the overall frontpage of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Pages/Home'
+import Footer from './Components/Footer'
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
 
 
       </div>
+      <Footer />
 
     </div>
   )
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import FeaturedDestination from '../Components/FeaturedDestination'
+import ExclusiveOffers from '../Components/ExclusiveOffers'
+import Testimonials from '../Components/Testimonials'
+import NewsLetter from '../Components/NewsLetter'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <FeaturedDestination />
+        <ExclusiveOffers />
+        <Testimonials />
+        <NewsLetter />
       
     </div>
   )
